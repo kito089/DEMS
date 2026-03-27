58caa28214fd6ad34ac863fda7341ce7b295af05
ThreeBook3458 <aronhuertayan@gmail.com>
2026-03-27 09:41:09 -0600

    Login con madre 👌☆*: .｡. o(≧▽≦)o .｡.:*☆

--- DEMSFRONT/src/app/components/input/inputComponent.ts
+++ DEMSFRONT/src/app/components/input/inputComponent.ts
@@ -1,12 +1,29 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './input.html',
-  styleUrls: ['./input.css'],
+  styleUrls: ['./input.css']
 })
 export class InputComponent {
-  @Input() placeholder: string = '';
-  @Input() type: string = 'text';
+  @Input() label: string = '';
+  @Input() type: string = 'text'; // 'text', 'password', 'email', etc.
+  @Input() isPasswordToggle: boolean = false; // Nueva propiedad
+
+  hide = true;
+
+  togglePassword() {
+    this.hide = !this.hide;
+  }
+
+  // Determina el tipo actual del input dinámicamente
+  get inputType(): string {
+    if (this.isPasswordToggle) {
+      return this.hide ? 'password' : 'text';
+    }
+    return this.type;
+  }
 }
