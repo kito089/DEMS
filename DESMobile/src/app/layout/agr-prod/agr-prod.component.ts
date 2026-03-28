265bd09dfc3c4cf3239152a9717ad62f2e8c6a83
Julio <100489915+kito089@users.noreply.github.com>
2026-03-28 17:10:51 -0600

    ya jalo el puto modaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaal aunq con css culero

--- DESMobile/src/app/layout/agr-prod/agr-prod.component.ts
+++ DESMobile/src/app/layout/agr-prod/agr-prod.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-agr-prod',
+  selector: 'app-seleccionar-platillo',
+  standalone: true,
   templateUrl: './agr-prod.component.html',
   styleUrls: ['./agr-prod.component.scss'],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
 })
-export class AgrProdComponent  implements OnInit {
+export class SeleccionarPlatilloComponent {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) {}
 
-  ngOnInit() {}
+  platillos = [
+    { nombre: 'Enchiladas verdes', tipo: 'Alimento', precio: 105, img: 'https://via.placeholder.com/40' },
+    { nombre: 'Pozole rojo', tipo: 'Alimento', precio: 120, img: 'https://via.placeholder.com/40' },
+    { nombre: 'Agua de Jamaica', tipo: 'Bebida', precio: 30, img: 'https://via.placeholder.com/40' },
+    { nombre: 'Tacos dorados', tipo: 'Alimento', precio: 80, img: 'https://via.placeholder.com/40' },
+    { nombre: 'Coca Cola', tipo: 'Bebida', precio: 40, img: 'https://via.placeholder.com/40' },
+  ];
 
+  cerrar() {
+    this.modalCtrl.dismiss();
+  }
 }
