d9904e3dda0af3ddef0ca07778c16fd92e16da2e
anonlettuce <migthysheepdev@gmail.com>
2026-03-28 00:03:08 -0600

    comentarios breves en server

--- Windows/DEMSBACK/src/server.js
+++ Windows/DEMSBACK/src/server.js
@@ -8,3 +8,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor DEMS! corriendo en http://localhost:${PORT}`);
 });
+
+//Para probar este archivo con node server.js
+//se ejecuta node server.js desde la carpeta de src
