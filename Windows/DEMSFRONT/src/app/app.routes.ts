465751cb38414c1e7862bbcb80d4c77e8c5a6f6f
ThreeBook3458 <aronhuertayan@gmail.com>
2026-03-28 23:35:48 -0600

    al chile quedo el login jajajaja (borren cocina lo que tiene el css y html fue de prueba)

--- Windows/DEMSFRONT/src/app/app.routes.ts
+++ Windows/DEMSFRONT/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/LoginComponent';
 import { MenuComponent } from './pages/menu/menuComponent';
 import { InicioComponent } from './pages/inicio/inicioComponent';
-import { PedidosCocinaComponent } from './pages/cocina/cocinaComponent';
+import { CocinaComponent } from './pages/cocina/cocinaComponent';
+import { MeseroComponent } from './pages/mesero/meseroComponent';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  {path: 'inicio', component: InicioComponent},
-  {path: 'login', component: LoginComponent },
-  {path: 'cocina', component: PedidosCocinaComponent },  
-  { path: 'menu', component: MenuComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'cocina', component: CocinaComponent },
+  { path: 'mesero', component: MeseroComponent },
 ];
