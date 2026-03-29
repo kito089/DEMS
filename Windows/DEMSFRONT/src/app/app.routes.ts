90926fc3f4c4d422161de87ddcc19f0d27bacafa
ThreeBook3458 <aronhuertayan@gmail.com>
2026-03-28 22:27:36 -0600

    nose

--- Windows/DEMSFRONT/src/app/app.routes.ts
+++ Windows/DEMSFRONT/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/LoginComponent';
 import { MenuComponent } from './pages/menu/menuComponent';
 import { InicioComponent } from './pages/inicio/inicioComponent';
+import { PedidosCocinaComponent } from './pages/cocina/cocinaComponent';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   {path: 'inicio', component: InicioComponent},
+  {path: 'login', component: LoginComponent },
+  {path: 'cocina', component: PedidosCocinaComponent },  
   { path: 'menu', component: MenuComponent }
-  
 ];
