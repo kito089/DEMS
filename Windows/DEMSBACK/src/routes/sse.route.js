af04f0fabed95ccf1a45d391b3cdc3090183a5d1
anonlettuce <migthysheepdev@gmail.com>
2026-03-29 02:04:22 -0600

    rutas sse

---
+++ Windows/DEMSBACK/src/routes/sse.route.js
@@ -0,0 +1,38 @@
+// routes/sse.routes.js
+import { Router } from 'express';
+
+const router = Router();
+
+let clients = [];
+
+router.get('/events', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    res.flushHeaders();
+
+    const clientId = Date.now();
+
+    const newClient = {
+        id: clientId,
+        res
+    };
+
+    clients.push(newClient);
+
+    console.log('Cliente conectado SSE:', clientId);
+
+    req.on('close', () => {
+        console.log('Cliente desconectado:', clientId);
+        clients = clients.filter(c => c.id !== clientId);
+    });
+});
+
+export const sendEventToAll = (data) => {
+    clients.forEach(client => {
+        client.res.write(`data: ${JSON.stringify(data)}\n\n`);
+    });
+};
+
+export default router;
