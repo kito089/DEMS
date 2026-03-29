ba4666aebe93092744b55dfd806e11f0e295dd31
anonlettuce <migthysheepdev@gmail.com>
2026-03-29 13:19:24 -0600

    //npm install nodemon //npm run dev desde raíz

--- Windows/DEMSBACK/src/config/connection.js
+++ Windows/DEMSBACK/src/config/connection.js
@@ -1,29 +1,28 @@
 import sql from 'mssql';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const dbSettings = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  server: process.env.DB_SERVER, // ← debe ser STRING limpio
-  port: parseInt(process.env.DB_PORT),
-  options: {
-    encrypt: false,
-    trustServerCertificate: true
-  }
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    server:   process.env.DB_SERVER,
+    port:     parseInt(process.env.DB_PORT),
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
 };
 
 export const getConnection = async () => {
     try {
         const pool = await sql.connect(dbSettings);
-        console.log("Conexión a SQL Server establecida");
         return pool;
     } catch (error) {
-        console.error("Error de conexión a SQL Server:", error);
-        throw error; //IMPORTANTE Y CHAMAGOSO
+        console.error('Error de conexión a SQL Server:', error);
+        throw error;
     }
 };
 
 export { sql };
+
+//npm install nodemon
+//npm run dev desde raíz
