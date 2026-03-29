dc64e6f929c2def8aa2e884fd50e69e590aa842f
anonlettuce <migthysheepdev@gmail.com>
2026-03-29 02:04:22 -0600

    rutas sse

--- Windows/DEMSBACK/src/server.js
+++ Windows/DEMSBACK/src/server.js
@@ -1,5 +1,7 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import sseRoutes from './routes/sse.route.js';
+
 
 dotenv.config();
 
@@ -8,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor DEMS! corriendo en http://localhost:${PORT}`);
 });
+
+app.use('/sse', sseRoutes);
 
 //Para probar este archivo con node server.js
 //se ejecuta node server.js desde la carpeta de src
