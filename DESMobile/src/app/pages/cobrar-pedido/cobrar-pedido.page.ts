253fe31f022200f1cf4cd9fe3739fd2d69da60da
Julio <100489915+kito089@users.noreply.github.com>
2026-03-30 19:45:12 -0600

    ya esta la generacion de la pantalla de cobrar pedido @ThreeBook3458 rifate el css en lo q conecto con back y pongo modales (づ￣ 3￣)づ

--- DESMobile/src/app/pages/cobrar-pedido/cobrar-pedido.page.ts
+++ DESMobile/src/app/pages/cobrar-pedido/cobrar-pedido.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+
 import { PageBodyComponent } from '../../layout/page-body/page-body.component';
 import { PageHeaderCreamComponent } from '../../layout/page-header-cream/page-header-cream.component';
 import { BottomNavComponent } from '../../layout/bottom-nav/bottom-nav.component';
@@ -31,9 +33,32 @@ import { PrimaryButtonComponent } from '../../components/primary-button/primary-
   ],
 })
 export class CobrarPedidoPage implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  pedido: any;
+  total: number = 0;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const nav = this.router.getCurrentNavigation();
+    this.pedido = nav?.extras?.state?.['pedido'];
+
+    console.log('Pedido recibido:', this.pedido);
+
+    if (this.pedido) {
+      this.calcularTotal();
+    }
+  }
+
+  calcularTotal() {
+    this.total = this.pedido.items.reduce((acc: number, item: any) => {
+      return acc + (item.PrecioUnitario || 0);
+    }, 0);
+  }
+
+  getDescripcion(item: any): string {
+    return `${item.Cantidad}x ${item.nombre}${item.Nota ? ' (' + item.Nota + ')' : ''}`;
+  }
 
   noop(): void {}
 }
