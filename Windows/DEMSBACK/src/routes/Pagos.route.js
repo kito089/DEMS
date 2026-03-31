e3bef033a40cec85ccf95b645b117135814d659c
Julio <100489915+kito089@users.noreply.github.com>
2026-03-30 23:44:07 -0600

    funcionalidad de impresion de ticket, todavia no tengo impresora para probarlo ⊙﹏⊙∥

--- Windows/DEMSBACK/src/routes/Pagos.route.js
+++ Windows/DEMSBACK/src/routes/Pagos.route.js
@@ -10,5 +10,6 @@ router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getById);
 router.post('/', ctrl.create);
 router.post('/enviar-ticket', ctrl.enviarTicket);
+router.post('/imprimir-ticket', ctrl.imprimirTicket);
 
 export default router;
