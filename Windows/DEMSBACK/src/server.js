334693f0e257ebe9033e7cbd49306b7a8b71e700
anonlettuce <migthysheepdev@gmail.com>
2026-04-01 20:45:04 -0600

    fix de cocina, jala SSE y carga pedidos

--- Windows/DEMSBACK/src/server.js
+++ Windows/DEMSBACK/src/server.js
@@ -4,15 +4,10 @@ dotenv.config();
 import app from './app.js';
 import sseRoutes from './routes/sse.route.js';
 
-
+app.use('/sse', sseRoutes); 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor DEMS! corriendo en http://localhost:${PORT}`);
 });
-
-app.use('/sse', sseRoutes);
-
-//Para probar este archivo con node server.js
-//se ejecuta node server.js desde la carpeta de src
