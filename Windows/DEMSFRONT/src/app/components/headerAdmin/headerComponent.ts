da343f71d6994e9e8f62d6d9a558c1bda4f07feb
Julio <100489915+kito089@users.noreply.github.com>
2026-04-05 16:14:42 -0600

    otro pequeñisimo detalle (✿◡‿◡)

--- Windows/DEMSFRONT/src/app/components/headerAdmin/headerComponent.ts
+++ Windows/DEMSFRONT/src/app/components/headerAdmin/headerComponent.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.html',
   styleUrls: ['./header.css']
 })
@@ -16,6 +17,7 @@ export class HeaderComponent {
 
   ngOnInit() {
     this.rol = this.authService.getRol();
+    console.log('Rol en header:', this.rol);
   }
 
   logout() {
